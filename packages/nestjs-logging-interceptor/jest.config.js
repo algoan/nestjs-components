ef188a9811b4ba700a16a32625cf9bd2e1cd51df
Philippe Diep <philippe.diep@algoan.com>
2020-04-02 12:25:08 +0200

    test(jest): jest setup for nestjs express based app

---
+++ packages/nestjs-logging-interceptor/jest.config.js
@@ -0,0 +1,25 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+
+  testEnvironment: "node",
+
+  rootDir: "test",
+
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "json"
+  ],
+
+  // A map from regular expressions to paths to transformers
+  transform: {
+    "\\.(ts)$": "ts-jest"
+  },
+
+};
