c1de6837262a3247cb504596a874b80154365e8e
Dimosthenis Kalaitzis <32791127+DimosthenisK@users.noreply.github.com>
2023-02-13 10:24:53 +0200

    Add this in perPageName fix

--- packages/pagination/src/add-pagination-body.interceptor.ts
+++ packages/pagination/src/add-pagination-body.interceptor.ts
@@ -94,7 +94,7 @@ export class PaginationBodyInterceptor<T> implements NestInterceptor<DataToPagin
     project?: unknown,
   ): string => {
     // tslint:disable-next-line
-    let url = `${path}?page=${page}&${perPageName}=${limit}`;
+    let url = `${path}?page=${page}&${this.perPageName}=${limit}`;
 
     if (filter !== undefined) {
       url += `&filter=${filter}`;
