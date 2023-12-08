ec2142bdb60ab262f10fb565faa424bec2121950
Salim Ben Dakhlia <bendakhlia.salim@gmail.com>
2023-12-08 11:42:54 +0100

    refactor: refactor the new is enum function

--- packages/custom-decorators/src/is-enum.decorator.ts
+++ packages/custom-decorators/src/is-enum.decorator.ts
@@ -1,36 +1,7 @@
-import { buildMessage, isEnum, ValidateBy, ValidationOptions } from 'class-validator';
+import { IsEnum as OriginalIsEnum, ValidationOptions } from 'class-validator';
 
-const IS_ENUM = 'isEnum';
-
-/**
- * Returns the possible values from an enum (both simple number indexed and string indexed enums).
- */
-// eslint-disable-next-line
-function validEnumValues(entity: any): string[] {
-  return (
-    Object.entries(entity)
-      // @ts-ignore
-      // eslint-disable-next-line
-      .filter(([key, value]) => isNaN(parseInt(key)))
-      // @ts-ignore
-      .map(([key, value]) => value as string)
-  );
-}
-
-export function IsEnum(entity: object, validationOptions?: ValidationOptions): PropertyDecorator {
-  return ValidateBy(
-    {
-      name: IS_ENUM,
-      constraints: [entity, validEnumValues(entity)],
-      validator: {
-        validate: (value, args): boolean => isEnum(value, args?.constraints[0]),
-        defaultMessage: buildMessage(
-          (eachPrefix) =>
-            `${eachPrefix}$property has the value $value but must be one of the following values: $constraint2`,
-          validationOptions,
-        ),
-      },
-    },
-    validationOptions,
-  );
-}
+export const IsEnum = (entity: object, validationOptions?: ValidationOptions): PropertyDecorator =>
+  OriginalIsEnum(entity, {
+    message: '$property has the value $value but must be one of the following values: $constraint2',
+    ...validationOptions,
+  });
